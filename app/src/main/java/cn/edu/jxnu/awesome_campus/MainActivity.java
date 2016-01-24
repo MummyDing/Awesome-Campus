74354ede9a5291d9bf27ee968bdd6d333aee58e6
MummyDing <MummyDingz@gmail.com>
2016-01-24 16:08:06 +0800

    create Android project

---
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/MainActivity.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2016.  MummyDing & KevinWu
+ *    Awesome-Campus is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *    Awesome-Campus is distributed in the hope that it will be useful,but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ */
+
+package cn.edu.jxnu.awesome_campus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by MummyDing on 16-1-24.
+ * GitHub: https://github.com/MummyDing
+ * Blog: http://blog.csdn.net/mummyding
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
