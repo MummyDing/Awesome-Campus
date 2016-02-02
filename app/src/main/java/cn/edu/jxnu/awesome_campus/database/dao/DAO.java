525fff2ffe720135413030cec70f1a97cc50b42a
MummyDing <mummydingz@gmail.com>
2016-02-02 17:46:56 +0800

    add some comments to DAO

--- app/src/main/java/cn/edu/jxnu/awesome_campus/database/dao/DAO.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/database/dao/DAO.java
@@ -2,7 +2,6 @@ package cn.edu.jxnu.awesome_campus.database.dao;
 
 import java.util.List;
 
-import cn.edu.jxnu.awesome_campus.model.IModel;
 
 /**
  * Created by MummyDing on 16-2-2.
@@ -10,8 +9,29 @@ import cn.edu.jxnu.awesome_campus.model.IModel;
  * Blog: http://blog.csdn.net/mummyding
  */
 public interface DAO<M> {
+    /**
+     * 讲数据写入到数据库中
+     * @param list  数据源 需要写入的数据
+     * @return
+     */
     boolean cacheAll(List<M> list);
+
+    /**
+     * 清除缓存
+     * @return
+     */
     boolean clearCache();
+
+
+    /***
+     * 从数据库中获取数据
+     * @param list 如果获取到数据则添加到list中(添加前先clear)   失败则发送失败消息(EventBus)
+     */
     void loadFromCache(List<M> list);
+
+    /**
+     * 从网络拉取数据
+     * @param list 如果拉取成功则将拉取的数据添加到list中(添加前先clear)  失败则发送失败消息(EventBus)
+     */
     void loadFromNet(List<M> list);
 }
