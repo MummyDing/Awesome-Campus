62d4a526551a1512ebfe359bf7dfe7ddd815c7c7
Kevin Wu <wqd1993@qq.com>
2016-02-10 19:27:40 +0800

    Test Campus-News-WebView.

--- app/src/main/java/cn/edu/jxnu/awesome_campus/ui/home/CampusNewsDetailsActivity.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/ui/home/CampusNewsDetailsActivity.java
@@ -6,6 +6,10 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import cn.edu.jxnu.awesome_campus.R;
 import cn.edu.jxnu.awesome_campus.ui.base.SwipeBackActivity;
 import cn.edu.jxnu.awesome_campus.view.base.BaseView;
@@ -17,11 +21,12 @@ import cn.edu.jxnu.awesome_campus.view.base.BaseView;
  * Blog: http://blog.csdn.net/mummyding
  */
 
-public class CampusNewsDetailsActivity extends SwipeBackActivity implements BaseView{
+public class CampusNewsDetailsActivity extends SwipeBackActivity implements BaseView {
 
     private Toolbar toolbar;
     private TextView contentTitle;
     private WebView contentView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,8 +68,30 @@ public class CampusNewsDetailsActivity extends SwipeBackActivity implements Base
          * 测试用 非正式代码 ---By MummyDing
          */
         contentTitle.setText("江西师大广东校友会2015年会暨2016“遇见青春”众筹演唱会羊城首演");
-        String data = "<h2>Html</h2>"; //这里放html代码
-        contentView.loadDataWithBaseURL("file:///android_asset/", "<link rel=\"stylesheet\" type=\"text/css\" href=\"CampusNew.css\" />"+data, "text/html", "utf-8", null);
+        String data = importStr(); //这里放html代码
+        contentView.loadDataWithBaseURL("file:///android_asset/", "<link rel=\"stylesheet\" type=\"text/css\" href=\"CampusNew.css\" />" + data, "text/html", "utf-8", null);
 
+    }
+
+    /**
+     * 测试用：导入数据
+     *
+     * @author KevinWu
+     * create at 2016/2/10 18:20
+     */
+    private String importStr() {
+        InputStreamReader inputReader = null;
+        try {
+            inputReader = new InputStreamReader(getResources().getAssets().open("html_test.txt"));
+            BufferedReader bufReader = new BufferedReader(inputReader);
+            String line = "";
+            String result = "";
+            while ((line = bufReader.readLine()) != null)
+                result += line;
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
