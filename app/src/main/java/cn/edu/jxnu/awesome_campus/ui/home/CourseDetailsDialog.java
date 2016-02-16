a87a8e0ca9d86c5bb812c812fbbd5f15210dc7f7
Kevin Wu <wqd1993@qq.com>
2016-02-17 00:19:59 +0800

    Test CourseDetailsDialog.

--- app/src/main/java/cn/edu/jxnu/awesome_campus/ui/home/CourseDetailsDialog.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/ui/home/CourseDetailsDialog.java
@@ -1,11 +1,15 @@
 package cn.edu.jxnu.awesome_campus.ui.home;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.Browser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 import org.greenrobot.eventbus.EventBus;
@@ -13,10 +17,12 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import cn.edu.jxnu.awesome_campus.R;
+import cn.edu.jxnu.awesome_campus.database.spkey.EducationStaticKey;
 import cn.edu.jxnu.awesome_campus.event.EVENT;
 import cn.edu.jxnu.awesome_campus.event.EventModel;
 import cn.edu.jxnu.awesome_campus.model.home.CourseInfoModel;
 import cn.edu.jxnu.awesome_campus.support.urlconfig.Urlconfig;
+import cn.edu.jxnu.awesome_campus.support.utils.common.SPUtil;
 
 public class CourseDetailsDialog extends Activity {
 
@@ -48,12 +54,24 @@ public class CourseDetailsDialog extends Activity {
         courseID.setText(model.getCourseID());
         courseTeacher.setText(model.getCourseTeacher());
         courseClass.setText(model.getCourseClass());
-        classmateLink.setText(buildHtmlLink("Classmates", Urlconfig.Education_Classmate_Base_URL+model.getClassmateListLink()));
-        classForumLink.setText(buildHtmlLink("Forum",Urlconfig.Education_CourseForum_Base_URL+model.getClassForumLink()));
+//        classmateLink.setText(buildHtmlLink("Classmates", Urlconfig.Education_Classmate_Base_URL+model.getClassmateListLink()));
+//        classForumLink.setText(buildHtmlLink("Forum",Urlconfig.Education_CourseForum_Base_URL+model.getClassForumLink()));
+//        classmateLink.setMovementMethod(LinkMovementMethod.getInstance());
+//        classForumLink.setMovementMethod(LinkMovementMethod.getInstance());
 
-        classmateLink.setMovementMethod(LinkMovementMethod.getInstance());
-        classForumLink.setMovementMethod(LinkMovementMethod.getInstance());
-
+        classmateLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(Urlconfig.Education_Classmate_Base_URL+model.getClassmateListLink()));
+                Bundle bundle = new Bundle();
+                SPUtil sp=new SPUtil(CourseDetailsDialog.this);
+                bundle.putString("_ga", "GA1.3.609810117.1451115712");
+                bundle.putString("ASP.NET_SessionId", sp.getStringSP(EducationStaticKey.SP_FILE_NAME, EducationStaticKey.BASE_COOKIE));
+                bundle.putString("JwOAUserSettingNew", sp.getStringSP(EducationStaticKey.SP_FILE_NAME, EducationStaticKey.SPECIAL_COOKIE));
+                i.putExtra(Browser.EXTRA_HEADERS, bundle);
+                startActivity(i);
+            }
+        });
     }
 
     private Spanned buildHtmlLink(String text, String link){
