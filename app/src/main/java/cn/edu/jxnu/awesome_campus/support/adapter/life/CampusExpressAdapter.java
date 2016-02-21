1528524a7df055dd9c2560e46ba50ab3ce9bf4ae
MummyDing <mummydingz@gmail.com>
2016-02-21 17:48:58 +0800

    create CampusExpressDetails & test

--- app/src/main/java/cn/edu/jxnu/awesome_campus/support/adapter/life/CampusExpressAdapter.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/support/adapter/life/CampusExpressAdapter.java
@@ -1,15 +1,23 @@
 package cn.edu.jxnu.awesome_campus.support.adapter.life;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import org.greenrobot.eventbus.EventBus;
+
 import cn.edu.jxnu.awesome_campus.R;
+import cn.edu.jxnu.awesome_campus.event.EVENT;
+import cn.edu.jxnu.awesome_campus.event.EventModel;
+import cn.edu.jxnu.awesome_campus.model.home.CampusNewsModel;
 import cn.edu.jxnu.awesome_campus.model.life.CampusExpressModel;
 import cn.edu.jxnu.awesome_campus.support.adapter.BaseListAdapter;
+import cn.edu.jxnu.awesome_campus.ui.home.CampusNewsDetailsActivity;
+import cn.edu.jxnu.awesome_campus.ui.life.CampusExpressDetailsActivity;
 
 /**
  * Created by MummyDing on 16-2-20.
@@ -36,11 +44,20 @@ public class CampusExpressAdapter extends BaseListAdapter<CampusExpressModel,Cam
 
     @Override
     public void onBindViewHolder(VH holder, int position) {
-        CampusExpressModel model = getItem(position);
+        final CampusExpressModel model = getItem(position);
         holder.expressName.setText(model.getExpTextName());
         holder.workTime.setText(model.getWorkTime());
         holder.expressLoc.setText(model.getExpLoc());
         holder.expressTel.setText(model.getExpTel());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, CampusExpressDetailsActivity.class);
+                mContext.startActivity(intent);
+                EventBus.getDefault().postSticky(new EventModel<CampusExpressModel>(EVENT.SEND_MODEL_DETAIL,model));
+
+            }
+        });
     }
 
     class VH extends RecyclerView.ViewHolder{
