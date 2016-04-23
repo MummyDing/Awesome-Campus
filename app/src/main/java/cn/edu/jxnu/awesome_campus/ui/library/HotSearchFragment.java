a69b46f22f955c8ae44b732c0aab0ed8d536d623
MummyDing <mummydingz@gmail.com>
2016-04-23 13:35:01 +0800

    add some missing locale

--- app/src/main/java/cn/edu/jxnu/awesome_campus/ui/library/HotSearchFragment.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/ui/library/HotSearchFragment.java
@@ -1,8 +1,16 @@
 package cn.edu.jxnu.awesome_campus.ui.library;
 
+import android.os.Handler;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.edu.jxnu.awesome_campus.InitApp;
 import cn.edu.jxnu.awesome_campus.R;
 import cn.edu.jxnu.awesome_campus.ui.base.BaseListFragment;
+import me.next.tagview.TagCloudView;
 
 /**
  * Created by MummyDing on 16-2-1.
@@ -11,26 +19,36 @@ import cn.edu.jxnu.awesome_campus.ui.base.BaseListFragment;
  */
 public class HotSearchFragment extends BaseListFragment {
 
+    private TagCloudView tagTop;
+    private TagCloudView tagBottom;
+    private LinearLayout layout;
+    private ScrollView scrollView;
+    private final Handler mHandler = new Handler();
     @Override
     protected int getLayoutID() {
-        return super.getLayoutID();
+        return R.layout.fragment_hot_search;
     }
 
     @Override
     protected void init() {
-        super.init();
+        tagTop = (TagCloudView) parentView.findViewById(R.id.tag_top);
+        tagBottom = (TagCloudView) parentView.findViewById(R.id.tag_bottom);
+        layout = (LinearLayout) parentView.findViewById(R.id.layout);
+        scrollView = (ScrollView) parentView.findViewById(R.id.scrollView);
+
+        List<String> tags = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            tags.add("标签" + i);
+        }
+        tagTop.setTags(tags);
+        tagBottom.setTags(tags);
+        mHandler.post(ScrollRunnable);
     }
 
     @Override
     public String getTitle() {
         return InitApp.AppContext.getString(R.string.reading);
     }
-
-
-
-
-
-
 
 
 
@@ -52,5 +70,20 @@ public class HotSearchFragment extends BaseListFragment {
     public void initView() {
 
     }
-
+    private Runnable ScrollRunnable= new Runnable() {
+        @Override
+        public void run() {
+            int off = layout.getMeasuredHeight() - scrollView.getHeight();//判断高度
+            if (off > -50000) {
+                scrollView.scrollBy(0, 10);
+                if (scrollView.getScrollY() == off) {
+                    Thread.currentThread().interrupt();
+                    scrollView.scrollTo(0,0);
+                    mHandler.post(ScrollRunnable);
+                } else {
+                    mHandler.postDelayed(this, 5000);
+                }
+            }
+        }
+    };
 }
