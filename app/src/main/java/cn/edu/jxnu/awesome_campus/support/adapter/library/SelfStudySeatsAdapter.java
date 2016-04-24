229b8a1798f9b71a3a28dd675ce18b96db621373
KevinWu <wqd1993@qq.com>
2016-04-24 23:04:25 +0800

    Complete SelfStudySeatsAdapter.

--- app/src/main/java/cn/edu/jxnu/awesome_campus/support/adapter/library/SelfStudySeatsAdapter.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/support/adapter/library/SelfStudySeatsAdapter.java
@@ -1,10 +1,18 @@
 package cn.edu.jxnu.awesome_campus.support.adapter.library;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.ramotion.foldingcell.FoldingCell;
+
+import org.w3c.dom.Text;
+
+import cn.edu.jxnu.awesome_campus.R;
 import cn.edu.jxnu.awesome_campus.model.library.SelfStudySeatLeftModel;
 import cn.edu.jxnu.awesome_campus.support.adapter.BaseListAdapter;
 
@@ -27,18 +35,48 @@ public class SelfStudySeatsAdapter extends BaseListAdapter<SelfStudySeatLeftMode
 
     @Override
     public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.card_self_study_seat_left,parent,false);
+        VH vh = new VH(view);
+        return vh;
     }
 
     @Override
-    public void onBindViewHolder(VH holder, int position) {
-
+    public void onBindViewHolder(final VH holder, int position) {
+        SelfStudySeatLeftModel model=getItem(position);
+        holder.roomInfoTitle.setText(model.getRoomInfo());
+        holder.roomInfo.setText(model.getRoomInfo());
+        holder.freeSeat.setText(model.getFreeSeat());
+        holder.temporaryLeave.setText(model.getTemporaryLeave());
+        holder.totalSeat.setText(model.getTotalSeat());
+        holder.bePresent.setText(model.getBePresent());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.fc.toggle(false);
+            }
+        });
     }
 
     static class VH extends RecyclerView.ViewHolder{
-
+        View itemView;
+        TextView roomInfo;
+        TextView roomInfoTitle;
+        TextView bePresent;
+        TextView totalSeat;
+        TextView temporaryLeave;
+        TextView freeSeat;
+        FoldingCell fc;
         public VH(View itemView) {
             super(itemView);
+            this.itemView=itemView;
+            roomInfo=(TextView)itemView.findViewById(R.id.roomInfo);
+            roomInfoTitle=(TextView)itemView.findViewById(R.id.roomInfoTitle);
+            bePresent=(TextView)itemView.findViewById(R.id.bePresent);
+            totalSeat=(TextView)itemView.findViewById(R.id.totalSeat);
+            temporaryLeave=(TextView)itemView.findViewById(R.id.temporaryLeave);
+            freeSeat=(TextView)itemView.findViewById(R.id.freeSeat);
+            fc=(FoldingCell)itemView.findViewById(R.id.folding_cell);
+            fc.initialize(10, Color.TRANSPARENT, 0);
         }
     }
 }
