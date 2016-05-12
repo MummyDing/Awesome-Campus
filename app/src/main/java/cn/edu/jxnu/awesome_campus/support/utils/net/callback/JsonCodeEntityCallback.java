8b55269b7d5866ecc262b2ca837454b27d31b716
KevinWu <wqd1993@qq.com>
2016-05-12 22:44:01 +0800

    Consummate GoodsDetailActivity.

--- app/src/main/java/cn/edu/jxnu/awesome_campus/support/utils/net/callback/JsonCodeEntityCallback.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/support/utils/net/callback/JsonCodeEntityCallback.java
@@ -1,6 +1,9 @@
 package cn.edu.jxnu.awesome_campus.support.utils.net.callback;
 
+import android.util.Log;
+
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import com.squareup.okhttp.Headers;
 import com.squareup.okhttp.Response;
 
@@ -15,9 +18,14 @@ public abstract class JsonCodeEntityCallback<T> extends NetCallback {
     public abstract void onSuccess(T entity, int responseCode,Headers headers);
     public abstract void onFailure(String error);
     @Override
-    public void onResponse(Response response) throws IOException {
-        T entity = new Gson().fromJson(response.body().string(),((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
-        onSuccess(entity,response.code(),response.headers());
+    public void onResponse(Response response) throws IOException{
+        try{
+            T entity = new Gson().fromJson(response.body().string(),((ParameterizedType)getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+            onSuccess(entity,response.code(),response.headers());
+        }catch (JsonSyntaxException e){
+            Log.d("--","异常");
+            onFailure("数据解析出错");
+        }
     }
     @Override
     public void onFailure(final IOException e) {
