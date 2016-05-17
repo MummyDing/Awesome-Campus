c7ee271af4e08780ade5067fad501a601604e9a1
Thereisnospon <1183739890@qq.com>
2016-05-17 16:05:19 +0800

    finish jxnugo follow and unfollow

---
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/model/jxnugo/JxnuGoUnfollowRetBean.java
@@ -0,0 +1,16 @@
+package cn.edu.jxnu.awesome_campus.model.jxnugo;
+
+/**
+ * Created by yzr on 16/5/17.
+ */
+public class JxnuGoUnfollowRetBean {
+    private  String unfollowStatus;
+
+    public String getUnfollowStatus() {
+        return unfollowStatus;
+    }
+
+    public void setUnfollowStatus(String unfollowStatus) {
+        this.unfollowStatus = unfollowStatus;
+    }
+}
