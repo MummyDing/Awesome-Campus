08d3928df5e8bc013a0257618e5de0f4dc9fbb16
Thereisnospon <1183739890@qq.com>
2016-10-28 15:02:16 +0800

    fix chech version

--- app/src/main/java/cn/edu/jxnu/awesome_campus/support/utils/common/SystemUtil.java
+++ app/src/main/java/cn/edu/jxnu/awesome_campus/support/utils/common/SystemUtil.java
@@ -3,12 +3,18 @@ package cn.edu.jxnu.awesome_campus.support.utils.common;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Headers;
 
 import org.greenrobot.eventbus.EventBus;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import cn.edu.jxnu.awesome_campus.InitApp;
 import cn.edu.jxnu.awesome_campus.R;
@@ -48,6 +54,15 @@ public class SystemUtil {
         return getPackageInfo().versionName;
     }
 
+
+
+    public static boolean checkVersion(String versison){
+        String regex="\\d+\\.\\d+\\.\\d+";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(versison);
+        return matcher.matches();
+    }
+
     /**
      * 检查更新
      * @return
@@ -60,10 +75,14 @@ public class SystemUtil {
                         Log.d("目前版本",SystemUtil.getVersionName());
                         Log.d("远程版本",result);
                         Log.d("对比结果","--"+result.trim().compareTo(SystemUtil.getVersionName().toString()));
-                        if (SystemUtil.getVersionName().toString().equals(result.trim())) {
+
+
+
+
+                        if (checkVersion(result)&&SystemUtil.getVersionName().toString().equals(result.trim())) {
                             // 最新
                             EventBus.getDefault().post(new EventModel(EVENT.VERSION_CHECK_ALREADY_LATEST));
-                        } else if(result.trim().compareTo(SystemUtil.getVersionName().toString())>0){
+                        } else if(checkVersion(result)&&result.trim().compareTo(SystemUtil.getVersionName().toString())>0){
                             // 要更新
                             EventBus.getDefault().post(new EventModel<String>(EVENT.VERSION_CHECK_NEED_UPDATE, result));
                         }else{
